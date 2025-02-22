a6401d488efe4498498244663f92f41f3ee81bcc
William Kane <williamkane562@gmail.com>
2025-02-22 18:57:58 +0000

    Add a few questions

--- getNextQuestion.lua
+++ getNextQuestion.lua
@@ -1,6 +1,12 @@
-function getNextQuestion(flags)
+function getNextQuestion(index)
   local questions = {
-    "Example question 1?"
-    "Example question 2?"
+    "Are you good at driving?",
+    "Are you sure?",
+    "Do you speed?",
+    "Will you speed?",
+    "When will you speed?",
+    "Would you drive after 1 pint of beer?",
+    "After 2....?",
+    "Should you brake check a cyclist?"
   }
 end
