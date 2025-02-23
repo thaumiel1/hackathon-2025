d8e7115cfa1236a1ea9b8ab6275a61898d281b06
Chr1sKent <160389760+Chr1sKent@users.noreply.github.com>
2025-02-23 05:50:43 +0000

    Update main.lua

--- main.lua
+++ main.lua
@@ -34,20 +34,6 @@ local cheeseReplacement = {}
 local cheeseCounter = 1
 
 
-
-
---question list
-local questions = {
-  "Are you good at driving?",
-  "Are you sure?",
-  "Do you speed?",
-  "Will you speed?",
-  "When will you speed?",
-  "Would you drive after 1 pint of beer?",
-  "After 2....?",
-  "Should you brake check a cyclist?"
-}
-
 function love.keypressed(key, scancode, isrepeat)
   if key == "right" then
     state = state + 1
